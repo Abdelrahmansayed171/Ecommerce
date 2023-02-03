c9355063046fbcd6af55011cc52fd1af2c001c35
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-02-03 21:31:15 +0200

    i've created cards of the products but card need some modification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 
+import HomeScreen from './screens/HomeScreen.js'
+
 function App() {
   return (
     <div>
@@ -9,7 +11,7 @@ function App() {
       {/* // add some spacing using bootstrap which is imported from theme w've downloaded */}
       <main className= "py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer/>
