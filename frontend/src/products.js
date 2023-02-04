913e5140f66559b23a993101207094a3753f8119
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-02-04 16:12:10 +0200

    Front end has been done

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -2,7 +2,7 @@ const products = [
     {
         '_id': '1',
         'name': 'Airpods Wireless Bluetooth Headphones',
-        'image': 'images/airpods.jpg',
+        'image': '/images/airpods.jpg',
         'description': 'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality',
         'brand' : 'Apple',
         'category' : 'Electronics',
@@ -14,7 +14,7 @@ const products = [
     {
         '_id': '2',
         'name': 'iphone 11 pro 256GB',
-        'image': 'images/phone.jpg',
+        'image': '/images/phone.jpg',
         'description': 'Introducing the iphone 11 pro. A transformative triple-camera system with A13 Bionic processor',
         'brand' : 'Apple',
         'category' : 'Electronics',
@@ -26,7 +26,7 @@ const products = [
     {
         '_id': '3',
         'name': 'Cannon EOS 80D DSLR Camera',
-        'image': 'images/camera.jpg',
+        'image': '/images/camera.jpg',
         'description': 'Characterized by versatile imaging specs, the cannon EOS 80D further clarifies itself',
         'brand' : 'Cannon',
         'category' : 'Electronics',
@@ -38,7 +38,7 @@ const products = [
     {
         '_id': '4',
         'name': 'Sony Playstation 4 Pro',
-        'image': 'images/playstation.jpg',
+        'image': '/images/playstation.jpg',
         'description': 'The ultimate home entertainment center starts with PlayStation. Join your friendsor play offline',
         'brand' : 'Sony',
         'category' : 'Electronics',
@@ -50,7 +50,7 @@ const products = [
     {
         '_id': '5',
         'name': 'Logitech G-Series Gaming Mouse',
-        'image': 'images/mouse.jpg',
+        'image': '/images/mouse.jpg',
         'description': 'Get a better handle on your games with this Logitech LightSync gaming mouse.',
         'brand' : 'Logitech',
         'category' : 'Electronics',
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,17 +1,79 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 
 import Rating from '../components/Rating.js'
 
 import products from '../products.js'
 
-function ProductScreen({ match }) {
-    const product = products.find((p) => p._id == match.params.id)
-    
+function ProductScreen() {
+    let { id } = useParams();
+    const product = products.find((p) => p._id === id)
+
     return (
         <div>
-            {product.name}
+            <Link to='/' className='btn btn-secondary my-3'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid rounded/>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        
+                        <ListGroup.Item>
+                            <h3>
+                                {product.name}
+                            </h3>
+                        </ListGroup.Item>
+                        
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h5>Price: <strong>${product.price}</strong> </h5>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h6>Description: {product.description} </h6>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Price: 
+                                    </Col>
+                                    <Col>
+                                        <strong>${product.price}</strong> 
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col>
+                                        Status: 
+                                    </Col>
+                                    <Col>
+                                        {product.countInStock > 0
+                                            ? 'In Stock'
+                                            : 'Out Of Stock'
+                                        }
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            
+                            <ListGroupItem className='text-center'>
+                                <Button className='btn-block' type='button' disabled={product.countInStock > 0? false : true}>Add to cart</Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
