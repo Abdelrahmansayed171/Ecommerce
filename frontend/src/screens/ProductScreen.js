c425b1336a72ebc5c358c0e7c91c2a9301b3697b
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-02-04 13:42:00 +0200

    checkpoint

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 
-function ProductScreen() {
-  return (
-    <div>
-        Product
-    </div>
-  )
+import Rating from '../components/Rating.js'
+
+import products from '../products.js'
+
+function ProductScreen({ match }) {
+    const product = products.find((p) => p._id == match.params.id)
+    
+    return (
+        <div>
+            {product.name}
+        </div>
+    )
 }
 
 export default ProductScreen
